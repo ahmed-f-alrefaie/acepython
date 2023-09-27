426652c816f2c7cf08409f83c811bdf4f021c0cd
Ahmed <ahmed.al-refaie.12@ucl.ac.uk>
2023-09-27 12:20:49 +0100

    Initial commit

---
+++ python-ace/ace.py
@@ -0,0 +1,40 @@
+from .mdace import ace as acef
+
+import numpy as np
+import math
+import pkg_resources
+from astropy import units as u
+
+def run_ace(
+        temperature: u.Quantity,
+        pressure: u.Quantity,
+        He_solar: float = 10.93,
+        C_solar: float = 8.39,
+        O_solar: float = 8.73,
+        N_solar: float = 7.86,
+) -> np.ndarray:
+    """Run Ace"""
+
+    specfile = pkg_resources.resource_filename('python_ace','external/Data/composes.dat')
+    thermfile = pkg_resources.resource_filename('python_ace','external/Data/NASA.therm')
+
+
+    with open(specfile,'r') as f:
+        species = [l.split()[1].strip() for l in f if not l.split()[1].strip().endswith('c')]
+    with open(specfile,'r') as f:
+        molar_masses = np.array([float(l.split()[2].strip()) for l in f]) << u.u
+
+    mix_profile = acef(len(species),specfile,
+                                 thermfile,
+                                 np.zeros_like(pressure),
+                                 pressure.to(u.bar).value,
+                                 temperature.to(u.K).value,
+                                 He_solar,
+                                 C_solar,
+                                 O_solar,
+                                 N_solar)
+
+    mu_profile = (mix_profile*molar_masses[:,None]).sum(axis=0)
+
+    return species, mix_profile, mu_profile
+
