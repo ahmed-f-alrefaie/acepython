9a75afbc1d545f40c7af72069fed1797fe9e95b1
Ahmed <ahmed.al-refaie.12@ucl.ac.uk>
2025-02-15 15:55:26 +0000

    taurex3 tests pass

--- tests/test_ace.py
+++ tests/test_ace.py
@@ -19,3 +19,21 @@ def test_run_ace():
     assert mu_profile.shape[-1] == pressure.shape[-1]
 
     np.testing.assert_array_almost_equal(np.sum(mix_profile, axis=0), 1.0)
+
+
+def test_taurex3():
+    """Tests taurex3 to see if it runs."""
+    from acepython.taurex3 import ACEChemistry
+    from astropy import units as u
+    import numpy as np
+
+    temperature = np.linspace(3000, 1000, 100)
+    pressure = np.logspace(6, -2, 100)
+
+    tau = ACEChemistry()
+    tau.initialize_chemistry(temperature_profile=temperature, pressure_profile=pressure)
+
+    assert tau.mixProfile.shape[-1] == pressure.shape[-1]
+    assert tau.muProfile.shape[-1] == pressure.shape[-1]
+    
+
