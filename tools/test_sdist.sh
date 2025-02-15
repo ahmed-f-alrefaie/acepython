8f2e83977b36b869429cb01efef29110a022cdc0
Ahmed <ahmed.al-refaie.12@ucl.ac.uk>
2025-02-15 16:04:30 +0000

    Included taurex in sdist tests

--- tools/test_sdist.sh
+++ tools/test_sdist.sh
@@ -6,7 +6,7 @@ set -x
 python -m venv venv
 source venv/bin/activate
 python -m pip install --progress-bar=off dist/*.tar.gz
-pip install pytest
+pip install pytest taurex
 pytest tests/
 deactivate
 rm -r venv
